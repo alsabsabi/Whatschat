30876f091e61c3d2a9c903a9c5f4ca6d44a8f11b
alsabsabi <124681839+alsabsabi@users.noreply.github.com>
2026-08-23 02:21:38 +0300

    Clean up build.gradle.kts by removing unused sections
    Removed unnecessary buildscript and allprojects sections.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,4 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
 }
